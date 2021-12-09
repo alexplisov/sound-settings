a3df24fae2f135c8a4e25c3c2c0ebf8b0c0c26ec
Alexey Plisov <alexplisov@gmail.com>
2021-12-09 12:15:56 +0500

    Init slider current value from AudioServer

--- Setting/Setting.gd
+++ Setting/Setting.gd
@@ -11,15 +11,16 @@ var audio_stream_player: AudioStreamPlayer
 
 onready var play_button = $HBoxContainer/Play
 onready var stop_button = $HBoxContainer/Stop
+onready var bus_idx = AudioServer.get_bus_index(bus_name)
 
 
 func _ready():
 	$Label.text = label
 	audio_stream_player = get_node(audio_stream_player_path)
+	$HSlider.value = AudioServer.get_bus_volume_db(bus_idx)
 
 
 func _on_HSlider_value_changed(value):
-	var bus_idx = AudioServer.get_bus_index(bus_name)
 	if value > $HSlider.min_value:
 		AudioServer.set_bus_mute(bus_idx, false)
 		AudioServer.set_bus_volume_db(bus_idx, value)
